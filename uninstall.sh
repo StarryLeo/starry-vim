de1a09614d90071c6b55b15c71884a5c3d3a764f
StarryLeo <starryskymayuyu@gmail.com>
2018-08-28 11:27:50 +0800

    Add uninstall file

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+app_dir="$HOME/.starry-vim"
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+rm $HOME/.vimrc
+rm $HOME/.vimrc.plugs
+rm -rf $HOME/.vim
+
+rm -rf $app_dir
