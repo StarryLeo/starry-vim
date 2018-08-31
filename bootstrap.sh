43d447bbb6e7c96e69d2c6a979a04045ef203bc7
StarryLeo <starryskymayuyu@gmail.com>
2018-08-31 10:02:47 +0800

    Fix the progress bar stuck during install plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -163,11 +163,7 @@ setup_plug() {
 
     msg "Starting updating/installing plugins"
     
-    vim \
-
-        -u ~/.starry-vim/.vimrc.plugs.default \
-	"+PlugClean! | PlugInstall --sync" \
-	"+qall"
+    vim -u ~/.starry-vim/.vimrc.plugs.default +PlugClean! +PlugInstall --sync +qall
 
     export SHELL="$system_shell"
 
